098b9d24093a97caf0a0d51db1daddcad5052100
Benjamin BALET <benjamin.balet@gmail.com>
2018-03-15 09:17:57 +0700

    DB:Fix table prefix. Inject users

--- sql/skeleton.sql
+++ sql/skeleton.sql
@@ -14,6 +14,16 @@ CREATE TABLE IF NOT EXISTS `skeleton_users` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
+-- Dumping data for table `skeleton_users`
+--
+
+INSERT INTO `skeleton_users` (`id`, `firstname`, `lastname`, `login`, `email`, `password`, `role`, `active`) VALUES
+(1, 'Benjamin', 'BALET', 'bbalet', 'benjamin.balet@gmail.com', '$2a$08$LeUbaGFqJjLSAN7to9URsuHB41zcmsMBgBhpZuFp2y2OTxtVcMQ.C', 1, 1),
+(2, 'john', 'DOE', 'jdoe', 'jdoe@test.org', '$2a$08$Gk9WE1duEcKhEhxUKFmZteUU0sCZTgZIKkiPxhCe7yi0Jw0pBbDNW', 2, 1),
+(3, 'Bob', 'DENARD', 'bdenard', 'bdenard@test.org', '$2a$08$14jdHTPUZe5.zXxQ1NqhhO83xUt2Zkr.csGw10BH75B3VrJiNU8Bq', 2, 1),
+(5, 'Admin', 'ADMINISTRATOR', 'admin', 'admin@skeleton.org', '$2a$08$cnX6al6aTkoyh/N/tKZ11e8ec9J/sldA6R4NdP.2qhhDi0OD3ek1G', 1, 1);
+
+--
 -- Structure of table `roles`
 --
 CREATE TABLE IF NOT EXISTS `skeleton_roles` (
@@ -25,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `skeleton_roles` (
 --
 -- Content of table `roles`
 --
-INSERT INTO `roles` (`id`, `name`) VALUES
+INSERT INTO `skeleton_roles` (`id`, `name`) VALUES
 (1, 'admin'),
 (2, 'user'),
 (8, 'Super admin');
